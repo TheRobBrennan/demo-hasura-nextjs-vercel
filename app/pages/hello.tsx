e427868c89de0132948021955beabecc588ad194
Rob Brennan <rob@therobbrennan.com>
2020-07-07 22:42:46 -0700

    Crude implementation of Material UI in Hello page

--- app/pages/hello.tsx
+++ app/pages/hello.tsx
@@ -2,12 +2,19 @@ import { NextPage } from "next"
 
 import Header from "../components/Header"
 
+// Material UI
+import Container from "@material-ui/core/Container"
+import Box from "@material-ui/core/Box"
+
 const HelloPage: NextPage = () => {
   return (
-    <div>
-      <Header />
-      Hello from TypeScript!
-    </div>
+    <Container>
+      <Box my={4}>
+        <Header />
+        <hr />
+        Hello from TypeScript!
+      </Box>
+    </Container>
   )
 }
 export default HelloPage
