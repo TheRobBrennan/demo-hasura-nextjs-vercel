f6ca90b242d4ce5c38061faf0a2def1586ef1a10
Rob Brennan <rob@therobbrennan.com>
2020-07-07 22:39:14 -0700

    Added Material UI theme for SSR

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,15 +1,35 @@
 import Router from "next/router"
 
-import * as gtag from "../analytics/google-analytics"
-
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/App.css"
 
 // Google Analytics
+import * as gtag from "../analytics/google-analytics"
 Router.events.on("routeChangeComplete", (url) => gtag.pageview(url))
+
+// Material UI
+import { ThemeProvider } from "@material-ui/core/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import theme from "../material-ui/theme"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side")
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <Component {...pageProps}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Component>
+  )
 }
