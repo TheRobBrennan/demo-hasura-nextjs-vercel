6f604da5ae8331fabfcebe0e53d563cf12c15bda
Rob Brennan <rob@therobbrennan.com>
2020-07-07 22:30:59 -0700

    Added Material UI and Google Analytics

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/App.css'
+import Router from "next/router"
+
+import * as gtag from "../analytics/google-analytics"
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/App.css"
+
+// Google Analytics
+Router.events.on("routeChangeComplete", (url) => gtag.pageview(url))
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
